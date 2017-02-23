79c75681b29113f08501d36741fc86e3486540a7
r12a <ishida@w3.org>
2017-02-23 20:49:33 +0000

    initial draft of visual index

---
+++ index-data/label-list.js
@@ -0,0 +1,36 @@
+// Full list of labels in github to use as headers.
+// Nothing will be produced if there's no open issue for a given label.
+
+var labels = { 
+	'arabic':[],
+	'ethiopic':[],
+	'hebrew':[],
+	'japanese':[],
+	'mongolian':[],
+	'se-asian':[],
+	'tibetan':[],
+
+	'book':[],
+	'inscription':[],
+	'magazine':[],
+	'mockup':[],
+	'newsprint':[],
+	'signage':[],
+	'webpage':[],
+
+	'abbreviation':[],
+	'emphasis':[],
+	'font-style':[],
+	'fonts':[],
+	'hyphenation':[],
+	'initial-letter':[],
+	'justification':[],
+	'notes-footnotes':[],
+	'punctuation':[],
+	'quotations':[],
+	'ruby':[],
+	'text-decoration':[],
+	'vertical-text':[],
+	}
+
+
